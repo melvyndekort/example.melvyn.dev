d9ce82ebf720420c12cf8c713a1fb84b076dd3a4
Melvyn de Kort <melvyn@mdekort.nl>
2022-05-17 01:47:15 +0200

    Use cloudfront key groups for signing

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -2,6 +2,28 @@ resource "aws_cloudfront_origin_access_identity" "example" {
   comment = "Identity for example.melvyn.dev"
 }
 
+resource "aws_cloudfront_public_key" "example" {
+  comment     = "Public key for example.melvyn.dev"
+  encoded_key = <<EOF
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsxmpLgMJnOOzyf0INJVk
++1ryc2iwi+3TVcUWE1oFT4yniLoI4Ud7Vh9zLXXTSiUOuEkN9Gqe3jIxrjcxToxl
+f7IhvVWflTWVyFkkHlfG39ODZYNyo3xrUbOD9dZUBEsyIF7Z+M7R8ueFuZqylIZV
+sk22gVwZVXurPqWdny7jIbytY0fnaO+R1AyQcCVwncI+6eZmv0OD3d3mWJpSjXhc
+2WmqUKVZt+dOudlaFXxqjY9bKjqW4Xbh04+zJlfNG//1DVzamXkDufnitI4bA0Q2
+BsIs9oKWU2htUauDAkYQamwDrJEdbFwjrx8y1kmLoO9zDhrmTRUWPKwdpVLLnwTK
+aQIDAQAB
+-----END PUBLIC KEY-----
+EOF
+  name        = "example-melvyn-dev"
+}
+
+resource "aws_cloudfront_key_group" "example" {
+  comment = "Key group for example.melvyn.dev"
+  items   = [aws_cloudfront_public_key.example.id]
+  name    = "example-melvyn-dev"
+}
+
 data "aws_cloudfront_cache_policy" "caching_disabled" {
   name = "Managed-CachingDisabled"
 }
@@ -42,7 +64,7 @@ resource "aws_cloudfront_distribution" "example" {
     allowed_methods = ["GET", "HEAD"]
     cached_methods  = ["GET", "HEAD"]
 
-    trusted_signers = ["self"]
+    trusted_key_groups = [aws_cloudfront_key_group.example.id]
   }
 
   ordered_cache_behavior {
