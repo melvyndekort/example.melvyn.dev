87fc92fdd1837ebe94a70c26376dde87cd5506d7
Melvyn de Kort <melvyn@mdekort.nl>
2022-06-27 12:00:23 +0200

    Terraform format

--- terraform/state.tf
+++ terraform/state.tf
@@ -22,8 +22,8 @@ data "terraform_remote_state" "cloudsetup" {
   backend = "s3"
 
   config = {
-    bucket   = "mdekort.tfstate"
-    key      = "cloudsetup.tfstate"
-    region   = "eu-west-1"
+    bucket = "mdekort.tfstate"
+    key    = "cloudsetup.tfstate"
+    region = "eu-west-1"
   }
 }
